071b3011f64783d046633fb84ec6e3d9bbf04846
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-09-24 14:39:15 +0200

    fix: add type of variables in the migration for the types table

--- database/migrations/2024_09_24_122316_create_types_table.php
+++ database/migrations/2024_09_24_122316_create_types_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
-            $table->name();
+            $table->string('name', 100);
             $table->timestamps();
         });
     }
