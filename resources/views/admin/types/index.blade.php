ce6fcbd34c8aaeb199a5b6a6cdf3d09651816254
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-09-24 16:16:26 +0200

    fix: error typo "nome"

--- resources/views/admin/types/index.blade.php
+++ resources/views/admin/types/index.blade.php
@@ -3,6 +3,16 @@
 @section('content')
 <div class="container my-5">
     <div class="table-responsive">
+        <h1>Aggiungi/modifica le tipologie dei progetti</h1>
+        <form action="{{route('admin.types.store')}}" method="POST">
+            @csrf
+            <input type="text" name="name">
+            <button type="sibmit">Aggiungi</button>
+
+        </form>
+
+
+
         <table class="table">
             <tbody>
                 @foreach ($types as $type )
